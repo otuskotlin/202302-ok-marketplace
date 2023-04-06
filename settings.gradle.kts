0c3abe38aa44bbde002404c8f36e54d31186798c
Sergey Okatov <sokatov@gmail.com>
2023-04-06 10:33:25 +0500

    m2l3 Build fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 //include("m1l4-dsl")
 //include("m1l5-coroutines")
 //include("m1l6-flows-and-channels")
-include("m1l7-kmp")
+//include("m1l7-kmp")
 
 include("ok-mkpl-acceptance")
 
