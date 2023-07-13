6d6890cb9215e885b7aa3708e5f02a954f585059
roman.gaponov <roman.gaponov@payallps.com>
2023-07-13 22:54:32 +0300

    add timeout

--- ok-marketplace-repo-gremlin/src/test/kotlin/ArcadeDbContainer.kt
+++ ok-marketplace-repo-gremlin/src/test/kotlin/ArcadeDbContainer.kt
@@ -3,7 +3,7 @@ package ru.otus.otuskotlin.marketplace.backend.repository.gremlin
 import org.testcontainers.containers.GenericContainer
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
-
+import java.time.Duration
 
 object ArcadeDbContainer {
     val username: String = "root"
@@ -20,6 +20,7 @@ object ArcadeDbContainer {
 //            withEnv("arcadedb.server.plugins", "GremlinServer:com.arcadedb.server.gremlin.GremlinServerPlugin")
 //            withEnv("arcadedb.server.defaultDatabases", "OpenBeer[root]{import:https://github.com/ArcadeData/arcadedb-datasets/raw/main/orientdb/OpenBeer.gz}")
             waitingFor(Wait.forLogMessage(".*ArcadeDB Server started.*\\n", 1))
+            withStartupTimeout(Duration.ofMinutes(5))
             start()
             println("ARCADE: http://${host}:${getMappedPort(2480)}")
             println("ARCADE: http://${host}:${getMappedPort(2424)}")
